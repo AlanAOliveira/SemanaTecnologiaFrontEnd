b55eb487ffc44fb84b84375cac76736486b4f59e
MyNameIsGustavo <gustavo.professional.contact@gmail.com>
2024-09-01 19:20:32 -0300

    feat:: configurando o axios

---
+++ src/services/api.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+export const api = axios.create({
+    /* Digite aqui o seu endereço de IPv4. O caminho é: src/services/api.ts */
+    baseURL: "http://192.168.0.106:3000/"
+});
